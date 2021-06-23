ca2d1ddcb51441bc3536af361a7f66945e6084ba
Iqbal <D.Iqbal@pi.de>
2021-06-23 16:44:37 +0200

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,34 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+
+        /*         WeatherForecastController controller = new WeatherForecastController();
+                [Fact]
+                public void GetReturnsMYname()
+                {
+                    //Given
+                    var returnValue = controller.Get();
+                    Assert.Equal("danya master", returnValue.ToString());
+                    //controller.GetAction(1);
+
+                    //When
+
+                    //Then
+                } */
+
+
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
